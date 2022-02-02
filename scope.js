803fde3e6fdd52ebc13d3ab14679d8408e9b8be3
magdline <elizadeena143@yahoo.com>
2022-02-02 11:29:11 +0000

    Added scope.js

---
+++ scope.js
@@ -0,0 +1,23 @@
+'use strict'
+function stat()
+{
+    let x="foo";
+    if (x == "foo")
+    {
+        let y="too";
+}
+console.log(x);
+//console.log(y);
+}
+stat();
+
+function dosomething()
+{
+    console.log(a);
+    console.log(foo());
+    let a =1;
+    function foo(){
+        return 2;
+    }
+}
+dosomething();
